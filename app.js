f6e5096816b1610cae6cd23e72235b581bcdc80d
13XAVI <tresorxavier16@gmail.com>
2023-02-09 10:13:36 +0200

    add a Script file

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const swaggerDoc = require('./routers/src/swagga')
 const cors = require("cors")
 const swaggerDocs = require('./routers/src/swagga')
 
+app.use(cors())
 app.use(express.json())
 
 
