1566139bd4431833f044774209bd6936bc66542d
13XAVI <tresorxavier16@gmail.com>
2023-02-14 17:16:56 +0200

    last commit

--- All/routers/user.js
+++ All/routers/user.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
-const express=require("express");
+const express = require("express");
 const bcrypt = require('bcrypt')
-const router=express.Router();
+const router = express.Router();
 const mongoose = require('mongoose')
 const User = require('../module/User')
 const bodyParser = require('body-parser')
@@ -13,121 +13,127 @@ const Blog = require("../module/Blog");
 
 
 
-router.post('/signup',async(req,res,next)=>{
-    
-    User.find({email: req.body.email})
-    .exec().then(user =>{
-            if(user.length===1){
+router.post('/signup', async (req, res, next) => {
+
+    await User.find({ email: req.body.email })
+        .exec().then(user => {
+            if (user.length === 1) {
                 res.status(200).json({
-                    message : 'Email Exist'
+                    message: 'Email Exist'
                 })
-            }else{
-                
-            
-                bcrypt.hash(req.body.password,10, (err,hash)=>{
-                    if(err){
+            } else {
+
+
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if (err) {
                         console.log(err)
                         return res.status(500).json({
-                            error:err
-        
-                        }); 
-                    }else{
-                        
+                            error: err
+
+                        });
+                    } else {
+
                         const user = new User({
-                            _id: new  mongoose.Types.ObjectId(),
-                            firstname:req.body.firstname,
-                            lastname:req.body.lastname,
+                            _id: new mongoose.Types.ObjectId(),
+                            firstname: req.body.firstname,
+                            lastname: req.body.lastname,
                             email: req.body.email,
                             password: hash
-                    });
-                    console.log(user);
-                    user.save().then(result =>{
-                        console.log(result)
-                         res.status(201).json(result)
-                    }).catch(err =>{
-                        console.log(err);
-                        res.status(500).json({
-                            error:err
-                        })
-                    });
-        
-                }
-            })
-        }
+                        });
+                        console.log(user);
+                        user.save().then(result => {
+                            console.log(result)
+                            res.status(201).json(result)
+                        }).catch(err => {
+                            console.log(err);
+                            res.status(500).json({
+                                error: err
+                            })
+                        });
+
+                    }
+                })
+            }
         }).catch(error => {
-          res.status(500).json({
-            error:error
-          })
-    })
+            res.status(500).json({
+                error: error
+            })
+        })
 });
 
 
-router.get("/all",async(req,res,next)=>{
+router.get("/all", async (req, res, next) => {
     try {
-        const blog= await User.find();
+        const blog = await User.find();
         res.status(200).json(blog)
     } catch (error) {
         res.status(404).json(error)
     }
 })
 
-router.post("/Login",async(req,res,next)=>{
+router.post("/Login", async (req, res, next) => {
     try {
-        const user = await User.findOne({email: req.body.email})
-        if(!user){
-            res.status({
-                message : " Not found user"
-            })    
+
+        const user = await User.findOne({ email: req.body.email })
+        console.log("try")
+
+        if (!user) {
+            console.log("try")
+            return res.status(200).json({ message: " Not found user" })
+
+        } else {
+            const result = bcrypt.compare(req.body.password, user.password)
+            if (!result) {
+                console.log(error)
+                res.status({
+                    message: "Error"
+                })
+            }
         }
-           result =  bcrypt.compare(req.body.password,user.password)
-        if(!result){
-            console.log(error)
-            res.status({
-                message: "Error"
-            })
-        }
-        const token = jwt.sign({id:user._id,email:user.email},`${process.env.JWT_SECRET}`)
+
+
+        const token = jwt.sign({ id: user._id, email: user.email }, `${process.env.JWT_SECRET}`)
         user.token = token
         res.json({
-            user,token
+            user, token
         })
     } catch (error) {
-        
+        console.log(error);
+        return res.status(500).json({ message: "error occured", error })
     }
-    User.findOne({email: req.body.email})
 
 
 })
 
-router.delete('/delete/:_id',(req,res,next)=>{
-    Blog.remove({_id : req.params.userId})
-    .exec()
-    .then(
-        result =>{
-            res.status(200).json({
-                message: "User Deleted !"
+router.delete('/delete/:_id', (req, res, next) => {
+    Blog.remove({ _id: req.params.userId })
+        .exec()
+        .then(
+            result => {
+                res.status(200).json({
+                    message: "User Deleted !"
+                })
+            }
+        )
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
             })
-        }
-    )
-    .catch( err =>{
-        console.log(err)
-        res.status(500).json({
-            error: err
         })
-    })
 })
-router.patch("/update/:id",async(req,res)=>{
-    
+router.patch("/update/:id", async (req, res) => {
+
     try {
-        const {id} = req.params;
-        const user= await User.findByIdAndUpdate(
+        const { id } = req.params;
+        const user = await User.findByIdAndUpdate(
             {
-                _id:id
+                _id: id
             },
             {
-                $set:req.body 
+                $set: req.body
             });
-            console.log(req.params);
+        console.log(req.params);
         res.status(200).json(user)
 
     } catch (error) {
@@ -136,7 +142,7 @@ router.patch("/update/:id",async(req,res)=>{
 })
 
 
-module.exports=router
+module.exports = router
 
 
 
