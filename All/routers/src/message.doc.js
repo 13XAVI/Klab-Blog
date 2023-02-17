76a06d38b8bec276d71214024ca033007ddc539f
13XAVI <tresorxavier16@gmail.com>
2023-02-17 08:43:33 +0200

    My Last change

--- All/routers/src/message.doc.js
+++ All/routers/src/message.doc.js
@@ -1,221 +1,222 @@
 const listAllMail = {
-    tags:['Message'],
-    description:"List all users",
-    
-    responses:{
-        200:{
-            description:"OK",
-            content:{
-                 "application/json":{
-                    type:'object',
-                    example:{
-                        status:"success",
-                        data:[]
+    tags: ['Message'],
+    description: "List all Mail",
+
+    responses: {
+        200: {
+            description: "OK",
+            content: {
+                "application/json": {
+                    type: 'object',
+                    example: {
+                        status: "success",
+                        data: []
                     }
-                 }
+                }
             }
         }
     }
 }
 
 const createMail = {
-tags:['Message'],
-description:"Create a Messsage",
-requestBody:{
-    content:{
-        "application/json":{
-            schema:{
-                type:"object",
-                properties:{
-                    firstname:{
-                        type:"string",
-                        description:"user name",
-                        example:"Tresor"
-                    },
-                    lastname:{
-                        type:"string",
-                        description:"user name",
-                        example:"xavier"
-                    },
-                    email:{
-                        type:"string",
-                        description:"user email",
-                        example:"tresorxavier@gmail.com"
-                    },
-                    password:{
-                        type:"string",
-                        description:"user password",
-                        example:"12345"
-                    },
-                    phone:{
-                        type:"Number",
-                        required:"0780000000000"
+    tags: ['Message'],
+    description: "Create a Messsage",
+    requestBody: {
+        content: {
+            "application/json": {
+                schema: {
+                    type: "object",
+                    properties: {
+                        firstname: {
+                            type: "string",
+                            description: "user name",
+                            example: "Tresor"
+                        },
+                        lastname: {
+                            type: "string",
+                            description: "user name",
+                            example: "xavier"
+                        },
+                        email: {
+                            type: "string",
+                            description: "user email",
+                            example: "tresorxavier@gmail.com"
+                        },
+                        password: {
+                            type: "string",
+                            description: "user password",
+                            example: "12345"
+                        },
+                        phone: {
+                            type: "Number",
+                            description: "user Password",
+                            example: "0780000000000"
+                        }
+                    }
+                }
+            }
+        }
+    },
+    responses: {
+        200: {
+            description: "OK",
+            content: {
+                "application/json": {
+                    type: "object",
+                    example: {
+                        status: "success",
+                        data: []
                     }
                 }
             }
         }
     }
-},
-responses:{
-    200:{
-        description:"OK",
-        content:{
-            "application/json":{
-                type:"object",
-                example:{
-                    status:"success",
-                    data:[]
-                }
-            }
-        }
-    }
-}
 }
 
 
 
 const getMailById = {
-tags:['Message'],
-description:"Get the Message by id",
-parameters:[
-    {
-        name:"id",
-        in:"path",
-        description:"id of user",
-        type:"string",
-        example:"63ee03da42688498f291dc70"
-    }
-],
-responses:{
-    200:{
-        description:"OK",
-        content:{
-             "application/json":{
-                type:'object',
-                example:{
-                    status:"success",
-                    data:[]
-                }
-             }
+    tags: ['Message'],
+    description: "Get the Message by id",
+    parameters: [
+        {
+            name: "id",
+            in: "path",
+            description: "id of user",
+            type: "string",
+            example: "63ee03da42688498f291dc70"
         }
-    }
-}
-}
-
-const deleteMailById = {
-tags:['Message'],
-description:"Delete the Message by id",
-security: [
-    {
-      token: [],
-    },
-],
-parameters:[
-    {
-        name:"id",
-        in:"path",
-        description:"id of Message",
-        type:"string",
-        example:"63ee03da42688498f291dc70"
-    }
-],
-responses:{
-    200:{
-        description:"OK",
-        content:{
-             "application/json":{
-                type:'object',
-                example:{
-                    status:"success",
-                    data:[]
-                }
-             }
-        }
-    }
-}
-}
-
-const updateMAilById = {
-tags:['Message'],
-description:"Update Message by id",
-security: [
-    {
-      token: [],
-    },
-],
-  parameters:[
-    {
-        name:"id",
-        in:"path",
-        description:"id of Message",
-        type:"string",
-        example:"63ee03da42688498f291dc70"
-    }
-],
-requestBody:{
-    content:{
-        "application/json":{
-            schema:{
-                type:"object",
-                properties:{
-                    firstname:{
-                        type:"string",
-                        description:"user name",
-                        example:"Tresor"
-                    },
-                    lastname:{
-                        type:"string",
-                        description:"user name",
-                        example:"xavier"
-                    },
-                    email:{
-                        type:"string",
-                        description:"user email",
-                        example:"tresorxavier@gmail.com"
-                    },
-                    password:{
-                        type:"string",
-                        description:"user password",
-                        example:"12345"
+    ],
+    responses: {
+        200: {
+            description: "OK",
+            content: {
+                "application/json": {
+                    type: 'object',
+                    example: {
+                        status: "success",
+                        data: []
                     }
                 }
             }
         }
     }
-},
-responses:{
-    200:{
-        description:"OK",
-        content:{
-            "application/json":{
-                type:"object",
-                example:{
-                    status:"success",
-                    data:[]
+}
+
+const deleteMailById = {
+    tags: ['Message'],
+    description: "Delete the Message by id",
+    security: [
+        {
+            token: [],
+        },
+    ],
+    parameters: [
+        {
+            name: "id",
+            in: "path",
+            description: "id of Message",
+            type: "string",
+            example: "63ee03da42688498f291dc70"
+        }
+    ],
+    responses: {
+        200: {
+            description: "OK",
+            content: {
+                "application/json": {
+                    type: 'object',
+                    example: {
+                        status: "success",
+                        data: []
+                    }
                 }
             }
         }
     }
 }
+
+const updateMAilById = {
+    tags: ['Message'],
+    description: "Update Message by id",
+    security: [
+        {
+            token: [],
+        },
+    ],
+    parameters: [
+        {
+            name: "id",
+            in: "path",
+            description: "id of Message",
+            type: "string",
+            example: "63ee03da42688498f291dc70"
+        }
+    ],
+    requestBody: {
+        content: {
+            "application/json": {
+                schema: {
+                    type: "object",
+                    properties: {
+                        firstname: {
+                            type: "string",
+                            description: "user name",
+                            example: "Tresor"
+                        },
+                        lastname: {
+                            type: "string",
+                            description: "user name",
+                            example: "xavier"
+                        },
+                        email: {
+                            type: "string",
+                            description: "user email",
+                            example: "tresorxavier@gmail.com"
+                        },
+                        password: {
+                            type: "string",
+                            description: "user password",
+                            example: "12345"
+                        }
+                    }
+                }
+            }
+        }
+    },
+    responses: {
+        200: {
+            description: "OK",
+            content: {
+                "application/json": {
+                    type: "object",
+                    example: {
+                        status: "success",
+                        data: []
+                    }
+                }
+            }
+        }
+    }
 }
 
-exports.MessageRouterDocs = {
-"/Mail/createMail":{
-    post:createMail,
-},
+export const MessageRouterDocs = {
+    "/Mail/createMail": {
+        post: createMail,
+    },
 
- "/Mail/AllMail":{
-    get:listAllMail,
-},
+    "/Mail/AllMail": {
+        get: listAllMail,
+    },
 
-"/Mail/deleteMail":{
-    delete:deleteMailById
-},
-"/Mail/updateMail/:id":{
-    patch:updateMAilById
-},
-"/Mail/Mail/:id":{
-    patch:getMailById
-}
+    "/Mail/deleteMail": {
+        delete: deleteMailById
+    },
+    "/Mail/updateMail/:id": {
+        patch: updateMAilById
+    },
+    "/Mail/Mail/:id": {
+        patch: getMailById
+    }
 
 };
